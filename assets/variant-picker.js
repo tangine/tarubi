e75aa38fb0098dd2281df7c682d4862479a3d477
汪承祥 <worklibo@foxmail.com>
2026-07-25 15:57:30 +0800

    完善

---
+++ assets/variant-picker.js
@@ -0,0 +1,19 @@
+import {Component} from "@theme/component";
+
+class VariantPicker extends Component{
+  constructor() {
+    super();
+  }
+
+  connectedCallback(){
+    this.addEventListener("change", this.onVariantChange.bind(this));
+  }
+
+  onVariantChange(event){
+    console.log("VariantPicker", event);
+  }
+}
+
+if(!customElements.get("variant-picker")) {
+  customElements.define("variant-picker", VariantPicker);
+}
